63ee697cf962bd53be7e1d6f689e3fe0b4f0481c
utshabkg <ranjanyouth@gmail.com>
2021-06-28 20:46:37 +0600

    Lab 3 of NS Sir

---
+++ lab asm/5.1.asm
@@ -0,0 +1,49 @@
+; x86 - 32 bit assembly
+
+.686
+.model flat, c
+include C:\masm32\include\msvcrt.inc
+includelib C:\masm32\lib\msvcrt.lib
+
+.stack 100h
+printf PROTO arg1:Ptr Byte, printlist: VARARG
+scanf PROTO arg2:Ptr Byte, inputlist: VARARG
+
+.data
+output_msg_format byte "%s", 0Ah, 0
+output_msg byte "Enter the number:", 0Ah, 0
+output_msg1 byte "POSITIVE", 0Ah, 0
+output_msg2 byte "ZERO", 0Ah, 0
+output_msg3 byte "NEGATIVE", 0Ah, 0
+
+
+; msg byte "Hello World", 0Ah, 0
+input_format byte "%d", 0
+number sdword ? ; sign double word -> 32 bits signed number
+
+.code
+main proc
+    INVOKE printf, ADDR output_msg_format,ADDR output_msg
+    INVOKE scanf, ADDR input_format, ADDR number
+
+    mov eax, number
+    cmp eax, 0
+
+    jl NEGATIVE
+    je ZERO
+    jg POSITIVE
+    jmp EXIT_
+
+NEGATIVE:
+    INVOKE printf, ADDR output_msg_format, ADDR output_msg3
+    jmp EXIT_
+ZERO:
+    INVOKE printf, ADDR output_msg_format, ADDR output_msg2
+    jmp EXIT_
+POSITIVE:
+    INVOKE printf, ADDR output_msg_format, ADDR output_msg1
+    jmp EXIT_
+EXIT_:
+    ret
+main endp
+end
